56d6c46439634f588a75f4bc03aaf8e9ff7172ab
sergiopantoja <sergio@sergiopantoja.com>
2012-12-19 16:24:03 -0500

    Removed Base URL from all files

--- assets/js/services.js
+++ assets/js/services.js
@@ -5,13 +5,13 @@
 angular.module('myhonorsServices', ['ngResource']).
 
 	factory('Post', function($resource) {
-		return $resource('http://thc.fiu.edu/myhonors/posts/discuss/:postId', {}, {
+		return $resource('posts/discuss/:postId', {}, {
 			get: {method:'GET', params:{postId:''}, isArray:true}
 		});
 	}).
 
 	factory('Events', function($resource) {
-		return $resource('http://thc.fiu.edu/myhonors/events/:action/:eventId/json', {}, {
+		return $resource('events/:action/:eventId/json', {}, {
 			get: {method: 'GET', params:{action: 'details'}},
 			query: {method:'GET', params:{action: 'browse'}, isArray:true}
 		});
