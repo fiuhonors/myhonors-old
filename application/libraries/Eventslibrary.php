7a485299f6f1c8c06ed53bc195d0e9b8bc1f15d4
sergiopantoja <sergio@sergiopantoja.com>
2013-01-03 14:52:45 -0500

    Created separate CI controller & library for Attendance

--- application/libraries/Eventslibrary.php
+++ application/libraries/Eventslibrary.php
@@ -11,38 +11,6 @@ class Eventslibrary {
 		$this->ci->load->model(array('guide'));
 	}
 
-	/**
-	 * Fetches all the events that a user has attended and the corresponding event information
-	 * @param  string|int $userid the user's identification number
-	 * @return array              an multidimensional array that contains the user's id, and 
-	 */
-	public function getAttendance($userid)
-	{
-		$query = $this->ci->guide->get('attendance', array('pid' => $userid));
-
-		$result = array(
-			'userid' => $userid,
-			'events' => array()
-		);
-
-		foreach ($query as $attended)
-		{
-			$event = $this->ci->guide->get('events', array('eid' => $attended['eid']), true);
-
-			if (!empty($event))
-			{
-				$result['events'][$event['eid']] = array(
-					'eid' => $event['eid'],
-					'type' => $event['type'],
-					'name' => $event['name'],
-					'date' => $event['begins'],
-				);
-			}
-		}
-
-		return $result;
-	}
-
 	public function getEvents($eventid = null)
 	{
 		if (isset($eventid))
