b40b260b8e01b4b1f1cfad95f09f6ded6b9f8029
sergiopantoja <sergio@sergiopantoja.com>
2013-01-03 10:25:22 -0500

    Added _remap function to CI Events Controller
    This allows us to pass parameters directly into the index function so our
    API can ask for resource/id instead of resource/index/id

--- application/controllers/events.php
+++ application/controllers/events.php
@@ -8,6 +8,24 @@ class Events extends CI_Controller {
 		$this->load->library(array('form_validation', 'eventslibrary'));
 	}
 
+	/**
+	 * This function is necessary for all controllers that act as "resources".
+	 * It allows us to pass parameters directly into the index function so our
+	 * API can ask for resource/id instead of resource/index/id
+	 * @param  mixed $method the first parameter in the URL
+	 */
+	public function _remap($method)
+	{
+		if (method_exists($this, $method))
+		{
+			$this->$method();
+		}
+		else
+		{
+			$this->index($method);
+		}
+	}
+
 	public function index($eventid = null)
 	{
 		if (isset($eventid))
