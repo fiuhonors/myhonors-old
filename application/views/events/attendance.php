68e648633f4bf96ad8aff034fa988a3e233e645b
sergiopantoja <sergio@sergiopantoja.com>
2013-01-17 19:32:56 -0500

    Remove unused attendance view

--- application/views/events/attendance.php
+++
@@ -1,9 +0,0 @@
-<?php echo validation_errors(); ?>
-<?php echo form_open('events/attendance'); ?>
-
-<p><strong>Panther ID:</strong></p>
-
-<input type="text" name="userid" value="" size="7" />
-<input type="submit" value="Submit" />
-
-<?=form_close();?>
