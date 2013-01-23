d443241183718e84d44cbeaf9a820cac8f97f61e
sergiopantoja <sergio@sergiopantoja.com>
2013-01-23 17:00:49 -0500

    (API) Move library functionality for events & attendance into API controller

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -11,36 +11,71 @@ class Api extends REST_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->library(array('form_validation', 'eventslibrary', 'attendancelibrary'));
+		$this->load->library('form_validation');
+		$this->load->model('guide');
 	}
 
+	/**
+	 * @todo clean GET input
+	 */
 	public function events_get()
 	{
 		if ($this->get('id'))
 		{
 			// grab a single event
-			$events = $this->eventslibrary->get($this->get('id'));
+			$result = $this->guide->get('events', array('eid' => $this->get('id')), true);
 		}
 		else
 		{
 			// grab all events
-			$events = $this->eventslibrary->get();
+			$result = $this->guide->get('events');
 		}
 
-		$this->response($events);
+		$this->response($result);
 	}
 
+	/**
+	 * @todo clean GET input
+	 */
 	public function attendance_get()
 	{
 		if ($this->get('id'))
 		{
-			$attendance = $this->attendancelibrary->get($this->get('id'));
+			$query = $this->guide->get('attendance', array('pid' => $this->get('id')));
+
+			$result = array(
+				'userid' => $this->get('id'),
+				'events' => array()
+			);
+
+			foreach ($query as $attended)
+			{
+				$event = $this->guide->get('events', array('eid' => $attended['eid']), true);
+
+				if (!empty($event))
+				{
+					$result['events'][$event['eid']] = array(
+						'eid' => $event['eid'],
+						'type' => $event['type'],
+						'name' => $event['name'],
+						'date' => $event['begins'],
+					);
+				}
+			}
 		}
 		else
 		{
-			$attendance = $this->attendancelibrary->get();
+			$query = $this->guide->get('attendance');
+
+			$result = array();
+
+			foreach ($query as $attended)
+			{
+				// generate desired structure
+				$result[$attended['pid']][$attended['eid']] = $attended['timestamp'];
+			}
 		}
 
-		$this->response($attendance);
+		$this->response($result);
 	}
 }
