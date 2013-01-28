afa6c5aa29398ed2a65e729d373e3fbf4e251416
sergiopantoja <sergio@sergiopantoja.com>
2013-01-28 12:29:03 -0500

    (profile) Add support for 'current major' profile field

--- schema.sql
+++ schema.sql
@@ -65,7 +65,7 @@ CREATE TABLE IF NOT EXISTS `user_profiles` (
   `user_id` int(11) NOT NULL,
   `fname` varchar(32) COLLATE utf8_bin NOT NULL,
   `lname` varchar(32) COLLATE utf8_bin NOT NULL,
-  `country` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `major` varchar(64) COLLATE utf8_bin NOT NULL,
   `website` varchar(255) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
