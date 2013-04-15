6881dcb70e13f0104b65b756386f9588bd360b1a
Sergio Pantoja <sergio@sergiopantoja.com>
2013-04-15 13:40:27 -0400

    (login) Complete working login system

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,148 @@
 'use strict';
 
+var appResolve = {
+	/**
+	 * This auth function gets executed on each page load of every route
+	 * that has a "resolve: appResolve" property attaced to its
+	 * $routeProvider.when() definition.
+	 *
+	 * It uses AngularJS's $q service ( http://docs.angularjs.org/api/ng.$q )
+	 * to first create a deferred object and promise. After this promise is
+	 * resolved (via FirebaseAuthClient's callback), the loginPromise.then()
+	 * callbacks are executed. These determine the value of the final promise,
+	 * which the application is waiting to get resolved before displaying any
+	 * content (see http://www.youtube.com/watch?v=Kr1qZ8Ik9G8 ).
+	 */
+	auth: function ($rootScope, $route, $q, FirebaseIO)
+	{
+		var deferred = $q.defer();
+		var loginPromise = deferred.promise;
+
+		var finalPromise = loginPromise.then(function(user) {
+			// grab user profile
+			FirebaseIO.child('user_profiles/' + user.id).on('value', function(snapshot) {
+				if (snapshot.val() === null) {
+					return rejectIt('NO_PROFILE_FOUND');
+				}
+
+				$rootScope.safeApply(function() {
+					$rootScope.profile = snapshot.val();
+				});
+
+				return snapshot.val();
+			});
+
+		}, function(error) {
+			return rejectIt(error);
+		});
+
+		// this value is determined by the requireLogin object property as defined
+		// in each module's $routeProvider.when() chain. this allows us to set the
+		// privacy of each page individually
+		var requireLogin = $route.current.$$route.requireLogin;
+
+		var rejectIt = function(message) {
+			// the if/else logic here allows us to conditionally render the template
+			// of the page because, if a promise is rejected, the template will not be rendered
+			if (requireLogin) {
+				return $q.reject(message);
+			}
+			else {
+				return message;
+			}
+		};
+
+		var authClient = new FirebaseAuthClient(FirebaseIO, function(error, user) {
+			if (error) {
+				// an error occurred while attempting login
+				deferred.reject(error);
+			}
+			else if (user) {
+				// user authenticated with Firebase
+				$rootScope.safeApply(function() {deferred.resolve(user)});
+			}
+			else {
+				// user is logged out
+				deferred.reject('NOT_LOGGED_IN');
+			}
+		});
+
+		return finalPromise;
+	}
+};
+
 angular.module('myhonors', ['myhonorsConfig', 'Firebase', 'myhonorsUser', 'myhonorsEvents']).
 
 config(['$routeProvider', function($routeProvider) {
-	$routeProvider.otherwise({redirectTo:''});
+	$routeProvider.
+		when('/home', {template: 'Welcome home!', requireLogin: true, resolve: appResolve}).
+		otherwise({redirectTo:'/home'});
 }]).
 
-controller('AppCtrl', ['$scope', '$rootScope', '$location', 'Profile', function AppCtrl($scope, $rootScope, $location, Profile) {
-	$rootScope.page_title = "";
-	$rootScope.profile = Profile;
-
-	$scope.results = function(content) {
-		$scope.response = content;
+controller('AppCtrl', ['$scope', '$rootScope', 'FirebaseIO', '$route', '$location', function AppCtrl($scope, $rootScope, FirebaseIO, $route, $location) {
+	// AngularJS workaround for certain callbacks.
+	// see https://coderwall.com/p/ngisma
+	$rootScope.safeApply = function(fn) {
+		var phase = this.$root.$$phase;
+		if(phase == '$apply' || phase == '$digest') {
+			if(fn && (typeof(fn) === 'function')) {
+				fn();
+			}
+		} else {
+			this.$apply(fn);
+		}
 	};
+
+	$rootScope.login = {email: '', password: '', error: ''};
+
+	var authClient = new FirebaseAuthClient(FirebaseIO, function(error, user) {
+		if (error) {
+			// an error occurred while attempting login
+			alert(error);
+		}
+		else if (user) {
+			// user is logged in, redirect to homepage
+			$rootScope.safeApply(function() {
+				$location.path('');
+			});
+		}
+		else {
+			// user is logged out
+			// do nothing
+		}
+	});
+
+	$rootScope.doLogin = function() {
+		authClient.login('password', {
+			email: $rootScope.login.email,
+			password: $rootScope.login.password
+		});
+
+		$rootScope.login.password = '';
+		// $rootScope.login.error = '';
+	};
+	
+	$rootScope.doLogout = function() {
+		authClient.logout(); 
+		$rootScope.profile = null;
+		$location.path('/login');
+	};
+
+	$rootScope.profile = null;
+
+	$rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+		switch (rejection)
+		{
+			case 'NO_PROFILE_FOUND':
+			case 'NOT_LOGGED_IN':
+			default:
+				console.log(rejection);
+				$location.path('login');
+				break;
+		}
+	});
+
+	$rootScope.page_title = "";
 }]).
 
 directive('imgRotate', function($timeout) {
--- assets/js/user.js
+++ assets/js/user.js
@@ -4,87 +4,24 @@ angular.module('myhonorsUser', []).
 
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
-		when('/login', {templateUrl: 'assets/partials/login.html', controller: 'LoginCtrl'}).
-		when('/signup', {templateUrl: 'assets/partials/signup.html', controller: 'SignupCtrl'}).
-		when('/profile/:userId', {templateUrl: 'assets/partials/profile.html'}).
-		when('/profile/:userId/edit', {templateUrl: 'assets/partials/profile-edit.html', controller: 'ProfileEditCtrl'});
-
+		when('/login', {templateUrl: 'assets/partials/login.html', controller: 'LoginCtrl', requireLogin: false, resolve: appResolve}).
+		when('/signup', {templateUrl: 'assets/partials/signup.html', controller: 'SignupCtrl', requireLogin: false, resolve: appResolve}).
+		when('/profile/:userId', {templateUrl: 'assets/partials/profile.html', requireLogin: true, resolve: appResolve}).
+		when('/profile/:userId/edit', {templateUrl: 'assets/partials/profile-edit.html', controller: 'ProfileEditCtrl', requireLogin: true, resolve: appResolve});
 }]).
 
-factory('Profile', function() {
-	var profileObject = {};
-	return profileObject;
-}).
-
-factory('FirebaseAuth', function($rootScope, FirebaseIO) {
-	var dataObject = {};
-	var authClient = new FirebaseAuthClient(FirebaseIO, function(error, user) {
-		if (error) {
-			// an error occurred while attempting login
-			// todo: implement error messages into HTML
-			alert('error: ' + error.message);
-		}
-		else if (user) {
-			// user authenticated with Firebase
-			console.log('logged in: ', user);
-			dataObject.user = user;
-			$rootScope.$apply();
-		}
-		else {
-			// user is logged out
-			console.log('user logged out');
-			dataObject.user = null;
-		}
-	});
-	
-	dataObject.login = function(provider, email, password) {
-		if (provider === 'password') {
-			authClient.login('password', {
-				email: email,
-				password: password
-			});
-		}
-	};
-	
-	dataObject.logout = function() {
-		authClient.logout();  
-	};
-	
-	return dataObject;
-}).
-
-controller('LoginCtrl', ['$scope', '$location', 'FirebaseIO', 'FirebaseAuth', 'Profile', function($scope, $location, FirebaseIO, FirebaseAuth, Profile) {
-	$scope.login = {email: '', password: ''};
-	$scope.auth = FirebaseAuth;
-	$scope.profile = Profile;
-	
-	$scope.$watch('auth.user', function() {
-		if ($scope.auth.user) {
-			FirebaseIO.child('user_profiles/' + $scope.auth.user.id).on('value', function(snapshot) {
-				$scope.$apply(function() {
-					$scope.profile = snapshot.val();
-					$location.path('/');
-				});
-			});
-		}
-	});
-	
-	$scope.doLogin = function() {
-		FirebaseAuth.login('password', $scope.login.email, $scope.login.password);
-		$scope.login.password = '';
-	};
-
-	$scope.doLogout = function() {FirebaseAuth.logout()};
-
+controller('LoginCtrl', ['$scope', '$rootScope', '$route', '$location', 'FirebaseIO', function($scope, $rootScope, $route, $location, FirebaseIO) {
+	$scope.login = $rootScope.login;
+	$scope.profile = $rootScope.profile;
 	//used for navigating between the Login box and the Forgot Password? box
-	$scope.currentPage = 'login';
+	$scope.currentPage = 'loginPage';
 }]).
 
 controller('SignupCtrl', ['$scope', function($scope) {
 	$scope.signup = {email: '', password: '', confirmPassword: '', pantherID: '', myAccountsPassword: ''};
 }]).
 
-controller('ProfileEditCtrl', ['$scope', '$rootScope', '$routeParams', '$location', 'Profile', function EventBrowseCtrl($scope, $rootScope, $routeParams, $location, Profile) {
+controller('ProfileEditCtrl', ['$scope', '$rootScope', '$routeParams', '$location', function EventBrowseCtrl($scope, $rootScope, $routeParams, $location) {
 	$scope.profileEdit = Profile.get({id: $routeParams.userId}, function(data) {console.log(data);});
 
 	$scope.profileSave = function() {
