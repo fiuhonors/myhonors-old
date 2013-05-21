32ac37b07a75e98a4ac2b853e6f58716c2a4250f
Sergio Pantoja <sergio@sergiopantoja.com>
2013-05-20 23:03:04 -0400

    (dashboard) Simplify DashboardCtrl and add child_removed event for swipeRef
    The code is much easier to follow now. Closes #24

--- assets/js/dashboard/DashboardCtrl.js
+++ assets/js/dashboard/DashboardCtrl.js
@@ -1,34 +1,44 @@
 'use strict';
 
-angular.module('myhonorsDashboard').controller('DashboardCtrl', ['$scope', '$rootScope', 'FirebaseIO', function($scope, $rootScope,FirebaseIO) {
-	$scope.eventTypes = null;
+angular.module('myhonorsDashboard').controller('DashboardCtrl', ['$scope', '$rootScope', 'FirebaseIO', function($scope, $rootScope, FirebaseIO) {
+	$scope.eventTally = {
+		// initialize the event types
+		honorshour: 0,
+		colloquia: 0,
+		excellence: 0,
+
+		add: function(type, amount) {
+			// safely increment or decrement (by passing in a negative amount) an event type
+			if (angular.isNumber(this[type])) {
+				this[type] += amount;
+			}
+		},
+		processEvent: function(eid, amount) {
+			var eventRef = FirebaseIO.child('events/' + eid + '/type');
+
+			eventRef.on('value', function(snapshot)
+			{
+				// snapshot.val() gives us the event type from Firebase
+				$rootScope.safeApply(function() {
+					$scope.eventTally.add(snapshot.val(), amount);
+				});
+			});
+		}
+	};
 
 	$scope.$watch('profile', function() {
 		if ($rootScope.profile) {
-			// used to hold the event types
-			var eventTypes = {};
-
 			// get the user's swipes
 			var swipeRef = FirebaseIO.child('swipes/' + $rootScope.profile.pid);
 
-			swipeRef.on('child_added', function(snapshot) {
-				// for each swipe, get the event information
-				var eventRef = FirebaseIO.child('events/' + snapshot.name() + '/type');
+			swipeRef.on('child_added', function(snapshot)
+			{
+				$scope.eventTally.processEvent(snapshot.name(), 1);
+			});
 
-				eventRef.on('value', function (snapshot) {
-					var type = snapshot.val();
-
-					// if no event of a certain type has been saved yet, intialize it with 0
-					// then increment the "number of attendances" for that type of event
-					eventTypes[type] = eventTypes[type] || 0;
-					eventTypes[type] += 1;
-
-					$rootScope.safeApply(function() {
-						$scope.honorshours = eventTypes.honorshour || 0;
-						$scope.colloquia = eventTypes.colloquia || 0;
-						$scope.excellence = eventTypes.excellence || 0;
-					})
-				})
+			swipeRef.on('child_removed', function(snapshot)
+			{
+				$scope.eventTally.processEvent(snapshot.name(), -1);
 			});
 		}
 	})
