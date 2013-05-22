8b99ce3cbe6aa3feb73d0a0c6d3ea755d1cd6b8a
Sergio Pantoja <sergio@sergiopantoja.com>
2013-05-22 14:55:23 -0400

    (EventBrowseCtrl) Add realtime RSVP functionality

--- assets/js/events/EventBrowseCtrl.js
+++ assets/js/events/EventBrowseCtrl.js
@@ -3,13 +3,64 @@
 angular.module('myhonorsEvents').controller('EventBrowseCtrl', ['$scope', '$rootScope', 'FirebaseIO', function ($scope, $rootScope, FirebaseIO) {
 	$scope.searchName = '';
 	$scope.searchType = '';
-	$scope.events = [];
+
+	$scope.events = {};
 	
 	var eventsRef = FirebaseIO.child('/events');
 
 	eventsRef.on('child_added', function(snapshot) {
+		// save the event object and add a property holding its ID so
+		// we can reference it in the scope (like in hasRSVP(eid), for example)
+		var e = snapshot.val();
+		e.id = snapshot.name();
+		e.rsvps = snapshot.child('rsvps').numChildren();
+
 		$rootScope.safeApply(function() {
-			$scope.events.push(snapshot.val());
+			$scope.events[e.id] = e;
 		});
 	});
+
+	eventsRef.on('child_changed', function(snapshot) {
+		var e = snapshot.val();
+		e.id = snapshot.name();
+		e.rsvps = snapshot.child('rsvps').numChildren();
+
+		$rootScope.safeApply(function() {
+			$scope.events[e.id] = e;
+		});
+	});
+
+	eventsRef.on('child_removed', function(snapshot) {
+		var e = snapshot.val();
+		e.id = snapshot.name();
+		e.rsvps = snapshot.child('rsvps').numChildren();
+
+		$rootScope.safeApply(function() {
+			$scope.events[e.id] = undefined;
+		});
+	});
+
+	$scope.hasRSVP = function(eid) {
+		return angular.isDefined($rootScope.profile.rsvps) && $rootScope.profile.rsvps[eid] === true;
+	};
+
+	$scope.addRSVP = function(eid) {
+		// add attendance info to event (so we can pull it from event page)
+		var eventRef = FirebaseIO.child('/events/' + eid + '/rsvps/' + $rootScope.profile.id);
+		eventRef.set(true);
+
+		// and add it to the user's profile (so we can see it on the user's page)
+		var userRef = FirebaseIO.child('/user_profiles/' + $rootScope.profile.id + '/rsvps/' + eid);
+		userRef.set(true);
+	};
+
+	$scope.removeRSVP = function(eid) {
+		// add attendance info to event (so we can pull it from event page)
+		var eventRef = FirebaseIO.child('/events/' + eid + '/rsvps/' + $rootScope.profile.id);
+		eventRef.remove();
+
+		// and add it to the user's profile (so we can see it on the user's page)
+		var userRef = FirebaseIO.child('/user_profiles/' + $rootScope.profile.id + '/rsvps/' + eid);
+		userRef.remove();
+	};
 }]);
