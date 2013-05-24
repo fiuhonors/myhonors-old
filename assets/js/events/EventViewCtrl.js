f229cc077007281825d1ae3ae0844de76f9bb55e
Sergio Pantoja <sergio@sergiopantoja.com>
2013-05-24 05:08:38 -0400

    (EventView) Add basic RSVP functionality

--- assets/js/events/EventViewCtrl.js
+++ assets/js/events/EventViewCtrl.js
@@ -1,6 +1,8 @@
 'use strict';
 
-angular.module('myhonorsEvents').controller('EventViewCtrl', ['$scope', '$routeParams', '$window', 'FirebaseIO', 'apikey_google', function EventViewCtrl($scope, $routeParams, $window, FirebaseIO, apikey_google) {
+angular.module('myhonorsEvents').controller('EventViewCtrl', ['$scope', '$rootScope', '$routeParams', '$window', 'FirebaseIO', 'apikey_google', function ($scope, $rootScope, $routeParams, $window, FirebaseIO, apikey_google) {
+	var mapLoaded = false;
+
 	$window.initializeMap = function() {
 		var latLng = new google.maps.LatLng($scope.event.location.lat, $scope.event.location.long);
 		// we use parseFloat() here so we can add the offset (otherwise the offset concatenates since the object is a string)
@@ -28,19 +30,73 @@ angular.module('myhonorsEvents').controller('EventViewCtrl', ['$scope', '$routeP
 		});
 	}
 
+	$scope.rsvps = [];
+
 	var eventRef = FirebaseIO.child('events/' + $routeParams.eventId);
 
 	eventRef.on('value', function(snapshot) {
+
+		// get the first and last names of everyone who rsvp'd
+		if (snapshot.child('rsvps') !== null) {
+			// reset
+			$scope.safeApply(function() {
+				$scope.rsvps = [];
+			});
+
+			snapshot.child('rsvps').forEach(function(childSnapshot) {
+				var fnameRef = FirebaseIO.child('user_profiles/' + childSnapshot.name() + '/fname');
+
+				fnameRef.on('value', function(nameSnapshot) {
+					$scope.safeApply(function() {
+						$scope.rsvps.push(nameSnapshot.val());
+					});
+				});
+			});
+		}
+
 		$scope.safeApply(function() {
 			$scope.event = snapshot.val();
+			$scope.event.id = snapshot.name();
+			$scope.event.rsvps = snapshot.child('rsvps').numChildren();
 		});
 
-		// asynchronously load the Google Maps API script
-		var script = document.createElement("script");
-		script.type = "text/javascript";
-		script.src = "https://maps.googleapis.com/maps/api/js?key=" + apikey_google + "&sensor=false&callback=initializeMap";
-		document.body.appendChild(script);
+		// this way we don't reload the script everytime the event changes
+		if (!mapLoaded) {
+			// asynchronously load the Google Maps API script
+			var script = document.createElement("script");
+			script.type = "text/javascript";
+			script.src = "https://maps.googleapis.com/maps/api/js?key=" + apikey_google + "&sensor=false&callback=initializeMap";
+			document.body.appendChild(script);
+
+			mapLoaded = true;
+		}
 	});
+
+	// todo: move the repeated code below into a service that's shared between EventViewCtrl and EventBrowseCtrl
+
+	$scope.hasRSVP = function(eid) {
+		return $rootScope.profile && $rootScope.profile.rsvps && $rootScope.profile.rsvps[eid] === true;
+	};
+
+	$scope.addRSVP = function(eid) {
+		// add attendance info to event (so we can pull it from event page)
+		var eventRef = FirebaseIO.child('/events/' + eid + '/rsvps/' + $rootScope.profile.id);
+		eventRef.set(true);
+
+		// and add it to the user's profile (so we can see it on the user's page)
+		var userRef = FirebaseIO.child('/user_profiles/' + $rootScope.profile.id + '/rsvps/' + eid);
+		userRef.set(true);
+	};
+
+	$scope.removeRSVP = function(eid) {
+		// add attendance info to event (so we can pull it from event page)
+		var eventRef = FirebaseIO.child('/events/' + eid + '/rsvps/' + $rootScope.profile.id);
+		eventRef.remove();
+
+		// and add it to the user's profile (so we can see it on the user's page)
+		var userRef = FirebaseIO.child('/user_profiles/' + $rootScope.profile.id + '/rsvps/' + eid);
+		userRef.remove();
+	};
 
 	$scope.renderMap = function(location) {	
 		return '<iframe width="950" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="' + location + '"></iframe>';
