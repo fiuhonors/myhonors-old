5a690248a45ce81b157bea10271d6b00dfd4860b
Sergio Pantoja <sergio@sergiopantoja.com>
2013-05-23 23:29:11 -0400

    (EventViewCtrl) Pull event data from Firebase

--- assets/js/events/EventViewCtrl.js
+++ assets/js/events/EventViewCtrl.js
@@ -1,49 +1,45 @@
 'use strict';
 
-angular.module('myhonorsEvents').controller('EventViewCtrl', ['$scope', '$routeParams', '$window', 'Events', 'apikey_google', function EventViewCtrl($scope, $routeParams, $window, Events, apikey_google) {
+angular.module('myhonorsEvents').controller('EventViewCtrl', ['$scope', '$routeParams', '$window', 'FirebaseIO', 'apikey_google', function EventViewCtrl($scope, $routeParams, $window, FirebaseIO, apikey_google) {
 	$window.initializeMap = function() {
-		var latLng = new google.maps.LatLng($scope.event.lat, $scope.event.lng);
-		var latLngOffset = new google.maps.LatLng($scope.event.lat, parseFloat($scope.event.lng) + 0.0015);
+		var latLng = new google.maps.LatLng($scope.event.location.lat, $scope.event.location.long);
+		// we use parseFloat() here so we can add the offset (otherwise the offset concatenates since the object is a string)
+		var latLngOffset = new google.maps.LatLng($scope.event.location.lat, parseFloat($scope.event.location.long) + 0.0015);
 
 		var mapOptions = {
 			zoom: 18,
 			center: latLngOffset,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
+			mapTypeId: google.maps.MapTypeId.SATELLITE
 		};
 
 		var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
+		// use the FIU map overlays
 		var buildingsLayer = new google.maps.KmlLayer('http://sites.fiu.edu/emap/layers/Buildings.kmz', 
 			{suppressInfoWindows: true, preserveViewport: true}
 		);
+
 		buildingsLayer.setMap(map);
 
 		var marker = new google.maps.Marker({
 			position: latLng,
 			map: map,
-			title: $scope.event.location_name
+			title: $scope.event.location.name
 		});
 	}
 
-	var eventId = $routeParams.eventId;
+	var eventRef = FirebaseIO.child('events/' + $routeParams.eventId);
 
-	Events.get({eventId: eventId}, function(data) {
-		//success
-		$scope.event = data;
+	eventRef.on('value', function(snapshot) {
+		$scope.safeApply(function() {
+			$scope.event = snapshot.val();
+		});
 
 		// asynchronously load the Google Maps API script
 		var script = document.createElement("script");
 		script.type = "text/javascript";
 		script.src = "https://maps.googleapis.com/maps/api/js?key=" + apikey_google + "&sensor=false&callback=initializeMap";
 		document.body.appendChild(script);
-
-		switch ($scope.event.type)
-		{
-			case "h": $scope.event.type = "Honors Hour"; break;
-			case "c": $scope.event.type = "Colloquia"; break;
-			case "e": $scope.event.type = "Excellence Lecture"; break;
-			case "a": $scope.event.type = "Miscellaneous"; break;
-		}
 	});
 
 	$scope.renderMap = function(location) {	
