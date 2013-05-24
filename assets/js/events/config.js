71798d604a002b884b441f74ee42f60cfa9b25d1
Sergio Pantoja <sergio@sergiopantoja.com>
2013-05-24 05:18:28 -0400

    (events/config) Maintain user_profile across page reloads

--- assets/js/events/config.js
+++ assets/js/events/config.js
@@ -2,8 +2,8 @@
 
 angular.module('myhonorsEvents').config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
-		when('/events', {templateUrl: 'assets/partials/events.html', controller: 'EventBrowseCtrl'}).
-		when('/events/:eventId', {templateUrl: 'assets/partials/events-view.html', controller: 'EventViewCtrl'}).
-		when('/events/add', {templateUrl: 'assets/partials/events-add.html', controller: 'EventViewCtrl'}).
-		when('/citizenship', {templateUrl: 'assets/partials/citizenship.html', controller: 'CitizenshipCtrl'});
+		when('/events', {templateUrl: 'assets/partials/events.html', controller: 'EventBrowseCtrl', requireLogin: true, resolve: appResolve}).
+		when('/events/:eventId', {templateUrl: 'assets/partials/events-view.html', controller: 'EventViewCtrl', requireLogin: true, resolve: appResolve}).
+		when('/events/add', {templateUrl: 'assets/partials/events-add.html', controller: 'EventViewCtrl', requireLogin: true, resolve: appResolve}).
+		when('/citizenship', {templateUrl: 'assets/partials/citizenship.html', controller: 'CitizenshipCtrl', requireLogin: true, resolve: appResolve});
 }]);
