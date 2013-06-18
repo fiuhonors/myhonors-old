eddb96ed14d4a37cce8fd47d8c77dc4011adf2d3
Sergio Pantoja <sergio@sergiopantoja.com>
2013-06-18 14:29:51 -0400

    (Arch/Admin) Include more data & show projects in clean, table format

--- assets/js/arch/ArchCtrl.js
+++ assets/js/arch/ArchCtrl.js
@@ -6,7 +6,23 @@ angular.module('myhonorsArch').controller('ArchCtrl', ['$scope', '$rootScope', '
 
 	if ($rootScope.profile.auth.isArchMod) {
 		var projectsRef = FirebaseIO.child('arch');
-		$scope.projects = FirebaseCollection(projectsRef);
+		$scope.projects = FirebaseCollection(projectsRef, {metaFunction: function(doAdd, data) {
+			// get student data
+			FirebaseIO.child('user_profiles/' + data.child('student').val()).once('value', function(studentSnapshot) {
+				// get advisor data. even though this and the student data call aren't dependent
+				// on each other, we still do this in the student data callback so we don't need
+				// to mess with multiple asyncronous requests at once. this might be changed later
+				FirebaseIO.child('user_profiles/' + data.child('advisor').val()).once('value', function(advisorSnapshot) {
+					// now we have everything we want, so execute doAdd() with the final combined data
+					doAdd(data, {
+						student: angular.extend(studentSnapshot.val(), {pid: studentSnapshot.name()}),
+						advisor: angular.extend(advisorSnapshot.val(), {pid: advisorSnapshot.name()})
+					});
+				});
+			});
+		}});
+
+
 		$scope.doAdd = function() {
 			var projectRef = FirebaseIO.child('arch').push({
 				name: $scope.newProject.name,
