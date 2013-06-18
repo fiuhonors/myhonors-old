977d9e3868226659c217029f78bf4101a1db2d80
Sergio Pantoja <sergio@sergiopantoja.com>
2013-06-18 12:23:45 -0400

    (Arch) Add functionality for Arch moderators

--- assets/js/arch/ArchCtrl.js
+++ assets/js/arch/ArchCtrl.js
@@ -1,14 +1,37 @@
 'use strict';
 
-angular.module('myhonorsArch').controller('ArchCtrl', ['$scope', '$rootScope', 'FirebaseIO', '$timeout', '$location', function($scope, $rootScope, FirebaseIO, $timeout, $location) {
+angular.module('myhonorsArch').controller('ArchCtrl', ['$scope', '$rootScope', 'FirebaseIO', 'FirebaseCollection', '$timeout', '$location', function($scope, $rootScope, FirebaseIO, FirebaseCollection, $timeout, $location) {
 	$scope.projects = [];
 
-	if ($rootScope.profile.archProjects) {
+	if ($rootScope.profile.auth.isArchMod) {
+		var projectsRef = FirebaseIO.child('arch');
+		$scope.projects = FirebaseCollection(projectsRef);
+		$scope.doAdd = function() {
+			var projectRef = FirebaseIO.child('arch').push({
+				name: $scope.newProject.name,
+				student: $scope.newProject.studentId,
+				advisor: $scope.newProject.advisorId
+			});
+
+			// set student info
+			FirebaseIO.child('user_profiles/' + $scope.newProject.studentId + '/archProjects/' + projectRef.name()).set(true);
+			FirebaseIO.child('user_profiles/' + $scope.newProject.studentId + '/fname').set($scope.newProject.studentFname);
+			FirebaseIO.child('user_profiles/' + $scope.newProject.studentId + '/lname').set($scope.newProject.studentLname);
+
+			// set advisor info
+			FirebaseIO.child('user_profiles/' + $scope.newProject.advisorId + '/archProjects/' + projectRef.name()).set(true);
+			FirebaseIO.child('user_profiles/' + $scope.newProject.advisorId + '/fname').set($scope.newProject.advisorFname);
+			FirebaseIO.child('user_profiles/' + $scope.newProject.advisorId + '/lname').set($scope.newProject.advisorLname);
+
+			// reset all inputs
+			$scope.newProject = {name: '', studentId: '', studentFname: '', studentLname: '', advisorId: '', advisorFname: '', advisorLname: ''}
+		}
+	} else if ($rootScope.profile.archProjects) {
 		angular.forEach($rootScope.profile.archProjects, function(value, key) {
 			FirebaseIO.child('arch/' + key).once('value', function(snapshot) {
 				$timeout(function() {
 					var project = snapshot.val();
-					project.id = snapshot.name();
+					project.$id = snapshot.name();
 
 					$scope.projects.push(project);
 				});
