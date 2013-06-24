2c7c7a1a3a91bfe569c04339091bb6ecf26edcb2
Sergio Pantoja <sergio@sergiopantoja.com>
2013-06-24 14:27:32 -0400

    (Arch) Add 'Edit Project' page

---
+++ assets/js/arch/ArchEditCtrl.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('myhonorsArch').controller('ArchEditCtrl', ['$scope', '$rootScope', '$routeParams', 'FirebaseIO', '$timeout', '$location', function($scope, $rootScope, $routeParams, FirebaseIO, $timeout, $location) {
+	var projectRef = FirebaseIO.child('arch/' + $routeParams.projectId);
+
+	projectRef.on('value', function(snapshot) {
+		// get student profile
+		FirebaseIO.child('user_profiles/' + snapshot.child('student').val()).once('value', function(studentSnapshot) {
+			// then get advisor profile
+			FirebaseIO.child('user_profiles/' + snapshot.child('advisor').val()).once('value', function(advisorSnapshot) {
+				// then save everything to scope
+				$timeout(function() {
+					$scope.project = snapshot.val();
+					$scope.project.student = studentSnapshot.val();
+					$scope.project.student.id = snapshot.child('student').val();
+					$scope.project.advisor = advisorSnapshot.val();
+					$scope.project.advisor.id = snapshot.child('advisor').val();
+					$scope.project.$id = snapshot.name();
+				});
+			});
+		});
+	});
+
+	$scope.doSaveChanges = function() {
+		// ...
+	};
+}]);
--- assets/js/arch/config.js
+++ assets/js/arch/config.js
@@ -5,5 +5,6 @@ angular.module('myhonorsArch').config(['$routeProvider', function($routeProvider
 		when('/arch', {templateUrl: 'assets/partials/arch.html', controller: 'ArchCtrl', requireLogin: true, resolve: appResolve}).
 		when('/arch/add', {templateUrl: 'assets/partials/arch-add.html', controller: 'ArchAddCtrl', requireLogin: true, resolve: appResolve}).
 		when('/arch/:projectId', {templateUrl: 'assets/partials/arch-view.html', controller: 'ArchViewCtrl', requireLogin: true, resolve: appResolve}).
+		when('/arch/:projectId/edit', {templateUrl: 'assets/partials/arch-edit.html', controller: 'ArchEditCtrl', requireLogin: true, resolve: appResolve}).
 		when('/arch/:projectId/contract', {templateUrl: 'assets/partials/arch-contract.html', controller: 'ArchContractCtrl', requireLogin: true, resolve: appResolve});
 }]);
