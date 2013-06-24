9f4c0fe5221fb7b5f048beb763db64c5347724b4
Sergio Pantoja <sergio@sergiopantoja.com>
2013-06-24 19:44:07 -0400

    (Arch/Edit) Finish 'Edit Project' functionality

--- assets/js/arch/ArchEditCtrl.js
+++ assets/js/arch/ArchEditCtrl.js
@@ -22,6 +22,34 @@ angular.module('myhonorsArch').controller('ArchEditCtrl', ['$scope', '$rootScope
 	});
 
 	$scope.doSaveChanges = function() {
-		// ...
+		projectRef.set({
+			name: $scope.project.name,
+			student: $scope.project.student.id,
+			advisor: $scope.project.advisor.id,
+			// otherInfo is data that's only needed for ARCH right now
+			otherInfo: {
+				studentMajor1: $scope.project.otherInfo.studentMajor1,
+				studentMajor2: $scope.project.otherInfo.studentMajor2,
+				studentPhone: $scope.project.otherInfo.studentPhone,
+				studentEmail: $scope.project.otherInfo.studentEmail,
+				advisorEmail: $scope.project.otherInfo.advisorEmail
+			}
+		});
+
+		// remove archProject from old student & advisor
+		FirebaseIO.child('user_profiles/' + $scope.originalProject.student.id + '/archProjects/' + projectRef.name()).remove();
+		FirebaseIO.child('user_profiles/' + $scope.originalProject.advisor.id + '/archProjects/' + projectRef.name()).remove();
+
+		// set student info
+		FirebaseIO.child('user_profiles/' + $scope.project.student.id + '/archProjects/' + projectRef.name()).set(true);
+		FirebaseIO.child('user_profiles/' + $scope.project.student.id + '/fname').set($scope.project.student.fname);
+		FirebaseIO.child('user_profiles/' + $scope.project.student.id + '/lname').set($scope.project.student.lname);
+
+		// set advisor info
+		FirebaseIO.child('user_profiles/' + $scope.project.advisor.id + '/archProjects/' + projectRef.name()).set(true);
+		FirebaseIO.child('user_profiles/' + $scope.project.advisor.id + '/fname').set($scope.project.advisor.fname);
+		FirebaseIO.child('user_profiles/' + $scope.project.advisor.id + '/lname').set($scope.project.advisor.lname);
+
+		$location.path('#/arch/' + $routeParams.projectId);
 	};
 }]);
