4c6b0970a80473114ac2c621b9998dfd14e4d5e1
Sergio Pantoja <sergio@sergiopantoja.com>
2013-06-25 12:26:17 -0400

    (Arch/Edit) Check if removing/setting archProject references is necessary
    This helps cut down on our bandwidth costs (even if only by a little bit) by
    reducing the number of writes to the server.

--- assets/js/arch/ArchEditCtrl.js
+++ assets/js/arch/ArchEditCtrl.js
@@ -44,19 +44,29 @@ angular.module('myhonorsArch').controller('ArchEditCtrl', ['$scope', '$rootScope
 			}
 		});
 
-		// remove archProject from old student & advisor
-		FirebaseIO.child('user_profiles/' + $scope.originalProject.student.id + '/archProjects/' + projectRef.name()).remove();
-		FirebaseIO.child('user_profiles/' + $scope.originalProject.advisor.id + '/archProjects/' + projectRef.name()).remove();
+		if ($scope.originalProject.student.id !== $scope.project.student.id || $scope.originalProject.advisor.id !== $scope.project.advisor.id) {
+			// remove archProject from old student & advisor
+			FirebaseIO.child('user_profiles/' + $scope.originalProject.student.id + '/archProjects/' + projectRef.name()).remove();
+			FirebaseIO.child('user_profiles/' + $scope.originalProject.advisor.id + '/archProjects/' + projectRef.name()).remove();
+			
+			// add archProject to new student & advisor
+			FirebaseIO.child('user_profiles/' + $scope.project.student.id + '/archProjects/' + projectRef.name()).set(true);
+			FirebaseIO.child('user_profiles/' + $scope.project.advisor.id + '/archProjects/' + projectRef.name()).set(true);
+		}
+
+		/* TODO: the following code can be removed once we have all the students and advisors in the database.
+		 * Ideally, the Arch moderators won't be able to edit user profile info (such a fname, lname, email)
+		 * and this information will simply be pre-loaded into the Arch project view panel. */
 
 		// set student info
-		FirebaseIO.child('user_profiles/' + $scope.project.student.id + '/archProjects/' + projectRef.name()).set(true);
 		FirebaseIO.child('user_profiles/' + $scope.project.student.id + '/fname').set($scope.project.student.fname);
 		FirebaseIO.child('user_profiles/' + $scope.project.student.id + '/lname').set($scope.project.student.lname);
 
 		// set advisor info
-		FirebaseIO.child('user_profiles/' + $scope.project.advisor.id + '/archProjects/' + projectRef.name()).set(true);
 		FirebaseIO.child('user_profiles/' + $scope.project.advisor.id + '/fname').set($scope.project.advisor.fname);
 		FirebaseIO.child('user_profiles/' + $scope.project.advisor.id + '/lname').set($scope.project.advisor.lname);
+
+		/* end todo ------------------------- */
 
 		$location.path('#/arch/' + $routeParams.projectId);
 	};
