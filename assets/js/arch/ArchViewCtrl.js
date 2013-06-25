b3a4a4442d951d0d5d951b9800083210bdc59400
Sergio Pantoja <sergio@sergiopantoja.com>
2013-06-25 11:50:39 -0400

    (feat/Arch) Add ability to delete projects, with confirmation modal

--- assets/js/arch/ArchViewCtrl.js
+++ assets/js/arch/ArchViewCtrl.js
@@ -4,6 +4,11 @@ angular.module('myhonorsArch').controller('ArchViewCtrl', ['$scope', '$rootScope
 	var projectRef = FirebaseIO.child('arch/' + $routeParams.projectId);
 
 	projectRef.on('value', function(snapshot) {
+		if (snapshot.val() === null) {
+			// object was deleted or it doesn't exist, so do nothing
+			return;
+		}
+
 		// get student profile
 		FirebaseIO.child('user_profiles/' + snapshot.child('student').val()).once('value', function(studentSnapshot) {
 			// then get advisor profile
@@ -20,4 +25,19 @@ angular.module('myhonorsArch').controller('ArchViewCtrl', ['$scope', '$rootScope
 			});
 		});
 	});
+
+	$scope.doDelete = function() {
+		// remove references from student & advisor profiles
+		FirebaseIO.child('user_profiles/' + $scope.project.student.id + '/archProjects/' + projectRef.name()).remove();
+		FirebaseIO.child('user_profiles/' + $scope.project.advisor.id + '/archProjects/' + projectRef.name()).remove();
+
+		// remove project data
+		projectRef.remove();
+
+		// close the modal
+		$('#deleteProjectModal').modal('hide');
+
+		// redirect to main page
+		$location.path('#/arch');
+	};
 }]);
