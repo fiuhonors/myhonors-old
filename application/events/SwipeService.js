6010aa6649041e2f5960d0af73046735cb25553c
Sergio Pantoja <sergio@sergiopantoja.com>
2013-07-12 18:59:35 -0400

    feat(Swipe) Display random greetings

--- application/events/SwipeService.js
+++ application/events/SwipeService.js
@@ -1,6 +1,30 @@
 'use strict'
 
 angular.module('myhonorsEvents').factory('SwipeService', function($q, FirebaseIO, FirebaseCollection, UserService) {
+	var greetings = [
+		'{0} has entered the building',
+		'{0} has arrived',
+		'{0} swiped in',
+		'Ladies and gentlemen, clap your hands for {0}!',
+		'Thanks for coming, {0}',
+		'What\'s up {0}! Good to see you'
+	];
+
+	if (!String.prototype.format) {
+		String.prototype.format = function() {
+			var args = arguments;
+			return this.replace(/{(\d+)}/g, function(match, number) { 
+				return typeof args[number] != 'undefined' ? args[number] : match;
+			});
+		};
+	}
+
+	var getRandomGreeting = function(name) {
+		// selects a random greeting, then emulates a printf function to format the
+		// greeting with the proper name
+		return greetings[Math.floor(Math.random() * greetings.length)].format(name);
+	};
+
 	return {
 		create: function(eventId, userId) {
 			if (!angular.isString(eventId) || !angular.isString(userId)) {
@@ -15,7 +39,8 @@ angular.module('myhonorsEvents').factory('SwipeService', function($q, FirebaseIO
 			return FirebaseCollection(swipeRef, {metaFunction: function(doAdd, swipeSnapshot) {
 				FirebaseIO.child('user_profiles/' + swipeSnapshot.name()).once('value', function(userSnapshot) {
 					var extraData = {
-						fname: userSnapshot.child('fname').val()
+						fname: userSnapshot.child('fname').val(),
+						greeting: getRandomGreeting(userSnapshot.child('fname').val())
 					};
 					doAdd(swipeSnapshot, extraData);
 				});
