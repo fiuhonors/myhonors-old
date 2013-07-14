86ef13cb99a914801c93088706e79955515b4e01
Sergio Pantoja <sergio@sergiopantoja.com>
2013-07-13 22:24:50 -0400

    feat(Swipe) Add bigger, better HTML input box

--- application/events/SwipeCtrl.js
+++ application/events/SwipeCtrl.js
@@ -2,6 +2,7 @@
 
 angular.module('myhonorsEvents').controller('SwipeCtrl', ['$scope', '$routeParams', 'SwipeService', function ($scope, $routeParams, SwipeService) {
 	$scope.data = {userId: ''};
+	$scope.event = EventService.read($routeParams.eventId);
 	$scope.swipes = SwipeService.list($routeParams.eventId);
 	$scope.doAdd = function() {
 		SwipeService.create($routeParams.eventId, $scope.data.userId);
