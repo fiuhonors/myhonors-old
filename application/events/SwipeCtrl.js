f7c0de43cb2f3cf2846f0c8ad969b5f10b307fd6
Sergio Pantoja <sergio@sergiopantoja.com>
2013-07-13 22:29:05 -0400

    feat(Swipe) Display error if swipe is invalid

--- application/events/SwipeCtrl.js
+++ application/events/SwipeCtrl.js
@@ -1,11 +1,23 @@
 'use strict';
 
-angular.module('myhonorsEvents').controller('SwipeCtrl', ['$scope', '$routeParams', 'SwipeService', function ($scope, $routeParams, SwipeService) {
+angular.module('myhonorsEvents').controller('SwipeCtrl', ['$scope', '$timeout', '$routeParams', 'UserService', 'EventService', 'SwipeService', function ($scope, $timeout, $routeParams, UserService, EventService, SwipeService) {
 	$scope.data = {userId: ''};
 	$scope.event = EventService.read($routeParams.eventId);
 	$scope.swipes = SwipeService.list($routeParams.eventId);
 	$scope.doAdd = function() {
-		SwipeService.create($routeParams.eventId, $scope.data.userId);
+		var userId = $scope.data.userId;
+		UserService.exists(userId, function(result) {
+			if (result === true) {
+				SwipeService.create($routeParams.eventId, userId);
+				$timeout(function() {
+					$scope.error = false;
+				});
+			} else {
+				$timeout(function() {
+					$scope.error = true;
+				});
+			}
+		});
 		$scope.data.userId = '';
 	};
 }]);
