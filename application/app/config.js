fb6889fb58f48d81295120217806b819e355ebc2
Sergio Pantoja <sergio@sergiopantoja.com>
2013-07-30 17:04:46 -0400

    feat(*) Redirect to Dashboard by default

--- application/app/config.js
+++ application/app/config.js
@@ -3,5 +3,5 @@
 angular.module('myhonors').config(['$routeProvider', function($routeProvider) {
 	// set global redirect, this is effectively where our users get sent if
 	// they try to a access a route that doesn't exist (like a 404 redirect)
-	$routeProvider.otherwise({redirectTo:'/arch'});
+	$routeProvider.otherwise({redirectTo:'/dashboard'});
 }]);
