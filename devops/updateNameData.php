af41d6c1cbf8b1e59907876fd87e596460401551
Sergio Pantoja <sergio@sergiopantoja.com>
2013-08-27 23:21:23 -0400

    feat(devops) Add tool to update users' fname/lname data from LDAP

---
+++ devops/updateNameData.php
@@ -0,0 +1,65 @@
+<?php
+
+include_once "../auth/FirebaseToken.php";
+require_once 'firebaseLib.php';
+
+////////////////////////////////////////////////////////////////////////////
+
+define("SERVER_ADDRESS", "");
+define("BEFORE_USERNAME", "");
+define("AFTER_USERNAME", "");
+define("FIREBASE_SECRET", "");
+define("FIREBASE_URL", ""); // must use https
+
+$ldaprdn  = "uid=#######,ou=XXXXX,dc=XXXXX,dc=XXXXX"; // ldap rdn or dn
+$ldappass = ""; // associated password
+
+////////////////////////////////////////////////////////////////////////////
+
+$tokenGen = new Services_FirebaseTokenGenerator(FIREBASE_SECRET);
+$token = $tokenGen->createToken(array(), array('admin' => true));
+$users = json_decode(file_get_contents(FIREBASE_URL . '/user_profiles/.json?auth=' . $token), true);
+
+$fb = new fireBase(FIREBASE_URL, $token);
+
+// connect to ldap server
+$ldapconn = ldap_connect(SERVER_ADDRESS);
+if ($ldapconn)
+{
+
+	// binding to ldap server
+	$ldapbind = ldap_bind($ldapconn, $ldaprdn, $ldappass);
+
+	// verify binding
+	if ($ldapbind)
+	{
+		foreach($users as $u)
+		{
+			$search = ldap_search($ldapconn, (BEFORE_USERNAME . $u["pid"] . AFTER_USERNAME), "(objectClass=*)", array("givenName", "sn"));
+			$data = ldap_get_entries($ldapconn, $search);
+
+			if ($u["fname"] == null || $u["lname"] == null) {
+				$path = "/user_profiles/" . $u["pid"];
+				$fb->set($path . "/fname", $data[0]["givenname"][0]);
+				$fb->set($path . "/lname", $data[0]["sn"][0]);
+				echo "fixed " . $u["pid"] . "<br />";
+				usleep(250000);
+			}
+			else {
+				echo "skipped " . $u["pid"] . "<br />";
+			}
+		}
+	}
+	else
+	{
+		echo "Could not bind to LDAP.";
+	}
+
+	ldap_unbind($ldapconn);
+}
+else
+{
+	echo "Could not connect to server.";
+}
+
+?>
