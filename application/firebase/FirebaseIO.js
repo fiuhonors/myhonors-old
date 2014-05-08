f0c8d52cf9e637847e416ff2c23bf2ce06f5012a
amb110395 <amb110395@gmail.com>
2014-05-08 12:58:31 -0400

    Update FirebaseIO.js again
    I reverted it back to point to the testing firebase...

--- application/firebase/FirebaseIO.js
+++ application/firebase/FirebaseIO.js
@@ -3,5 +3,5 @@
 angular.module('Firebase').factory('FirebaseIO', function() {
 	// we call this factory 'FirebaseIO' instead of just 'Firebase' to avoid any naming
 	// conflicts with the Firebase Javascript library
-	return new Firebase('https://fiumyhonors.firebaseio.com/');
+	return new Firebase('https://fiuhonorstechteam.firebaseio.com/');
 });
