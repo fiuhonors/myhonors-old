04471099f8eae84f9bca484ab44d5c9702fa5612
Alastair Paragas <alastairparagas@gmail.com>
2014-05-08 17:36:17 -0400

    Config.php changes so that I can login localhost to database - LDAP changes

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-	define("FIREBASE_SECRET", "");
-	define("FIREBASE_SETTINGS_URL", ""); // must use https
-	define("FIREBASE_VOLUNTEER_URL", "");
-	define("FIREBASE_USERS_URL", "");
-	define("FIREBASE_EVENTS_URL", "");
-
+    define("FIREBASE_SECRET", "MsmJ4QQsQkeBIJZAnpTD2Fw4iMmB42jGvGyRDozb");
+    define("FIREBASE_SETTINGS_URL", "https://fiuhonorstechteam.firebaseio.com/system_settings/");
+    define("FIREBASE_VOLUNTEER_URL", "https://fiuhonorstechteam.firebaseio.com/volunteerHours/");
+    define("FIREBASE_USERS_URL", "https://fiuhonorstechteam.firebaseio.com/user_profiles/");
+    define("FIREBASE_EVENTS_URL", "https://fiuhonorstechteam.firebaseio.com/events/");
+    define("FIREBASE_CAREERS_URL", "https://fiuhonorstechteam.firebaseio.com/careers/");
 ?>
