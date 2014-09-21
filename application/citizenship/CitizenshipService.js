e88b35d5733aff190b7d462c62f113bd060c0f3d
Alastair Paragas <alastairparagas@gmail.com>
2014-09-21 18:17:04 -0400

    Bug fix: Citizenship Points not displaying correctly

--- application/citizenship/CitizenshipService.js
+++ application/citizenship/CitizenshipService.js
@@ -3,157 +3,85 @@
     var angular = window.angular;
 
     
-    angular.module('myhonorsCitizenship').factory('CitizenshipService', ['FirebaseIO', 'UserService', 'FirebaseCollection', '$q', 'EventService', function (FirebaseIO, UserService, FirebaseCollection, $q, EventService) {
+	angular.module('myhonorsCitizenship').factory('CitizenshipService', ['FirebaseIO', 'UserService', '$q', 'SwipeService', function (FirebaseIO, UserService, $q, SwipeService) {
         
-        var types = {},
-            citizenship = {};
+		var types,
+			userEvents,
+			points;
         
-        var citizenshipFactory = {
-            
-            getTypes: function () {
-                
-                var defer = $q.defer();
-                
-                if (types.hasOwnProperty('types') && types.hasOwnProperty('enabledTypes') && types.hasOwnProperty('disabledTypes')) {
-                    defer.resolve(types);
-                    return defer.promise;
-                }
-                
-                FirebaseIO.child('system_settings/eventTypes').once('value', function (snapshot) {
-                    var citizenshipTypes = {},
-                        citizenshipTypesEnabled = {},
-                        citizenshipTypesDisabled = {};
-                    angular.forEach(snapshot.val(), function (value, key) {
-                        
-                        if (value.citizenship == undefined) {
-                            return;
-                        }
-                        
-                        var typeIsEnabled = value.citizenship.enabled == undefined ? true : value.citizenship.enabled,
-                            typePoints = value.citizenship.points || 0,
-                            typeMaxPoints = value.citizenship.maxPoints || 0;
+		var citizenshipFactory = {
+			
+			getTypes: function () {
+				var defer = $q.defer();
+				if (types != undefined) {
+					defer.resolve(types);
+					return defer.promise;
+				}
+				FirebaseIO.child('system_settings/eventTypes').once('value', function (snapshot) {
+					var citizenshipTypes = {},
+						citizenshipTypesEnabled = {},
+						citizenshipTypesDisabled = {};
+					angular.forEach(snapshot.val(), function (value, key) {	
+						if (value.citizenship == undefined) {
+							return;
+						}
+						var typeIsEnabled = value.citizenship.enabled == undefined ? true : value.citizenship.enabled,
+							typePoints = value.citizenship.points || 0,
+							typeMaxPoints = value.citizenship.maxPoints || 0;
 
-                        citizenshipTypes[key] = {
-                            enabled: typeIsEnabled,
-                            points: typePoints,
-                            maxPoints: typeMaxPoints
-                        };
-                        if (typeIsEnabled) {
-                            citizenshipTypesEnabled[key] = {
-                                points: typePoints,
-                                maxPoints: typeMaxPoints
-                            };
-                        } else {
-                            citizenshipTypesDisabled[key] = {
-                                points: typePoints,
-                                maxPoints: typeMaxPoints
-                            };
-                        }
-                    });
-                    
-                    types = {
-                        types: citizenshipTypes,
-                        enabledTypes: citizenshipTypesEnabled,
-                        disabledTypes: citizenshipTypesDisabled
-                    };
-                    
-                    defer.resolve(types);
-                    
-                });
-                
-                return defer.promise;
-                
-            },
-            
-            
-            
-            getUser: function (pantherId) {
-                
-                if (pantherId == undefined) {
-                    pantherId = UserService.profile.id;
-                }
-                
-                var defer = $q.defer();
-                
-                if (citizenship.hasOwnProperty('points') && citizenship.hasOwnProperty('events')) {
-                    defer.resolve(citizenship);
-                    return defer.promise;
-                }
-                
-                citizenshipFactory.getTypes().then(function (typesPromise) {
-                    
-                    FirebaseIO.child('user_profiles/' + pantherId + '/attendance').once('value', function (snapshot) {
-                        var individualEventsDefer = [],
-                            userEvents = {},
-                            eventTypes = typesPromise.enabledTypes,
-                            roomswipeEvents = {},
-                            roomswipeTypes = {'studyroomswipe': 'Study Room Swipe', 'bbclabswipe': 'BBC Lab Swipe'},
-                            citizenshipPoints = 0,
-                            roomswipePoints = 0;
-                        angular.forEach(snapshot.val(), function (eventDetails, eventId) {
-                            var individualEventDefer = $q.defer();
-                            FirebaseIO.child('events/' + eventId).once('value', function (snapshot) {
-                                var eventType = snapshot.val().types[0];
-                                if (!eventTypes[eventType]) {
-                                    return;
-                                }
-                                if (userEvents[eventType] === undefined) {
-                                    userEvents[eventType] = [];
-                                }
-                                userEvents[eventType].push({
-                                    'eventName': snapshot.val().name,
-                                    'eventId': eventId
-                                });
-                                individualEventDefer.resolve();
-                            });
-                            individualEventsDefer.push(individualEventDefer.promise);
-                        });
-                        angular.forEach(roomswipeTypes, function (swipeDetails, swipeType) {
-                            var individualEventDefer = $q.defer();
-                            FirebaseIO.child('events/' + swipeType + '/attendance/' + pantherId).once('value', function (snapshot) {
-                                if (snapshot.val() !== null) {
-                                    roomswipeEvents[swipeDetails] = snapshot.val();
-                                }
-                                individualEventDefer.resolve();
-                            });
-                            individualEventsDefer.push(individualEventDefer.promise);
-                        });
-                        
-                        $q.all(individualEventsDefer).then(function () {
-                            
-                            angular.forEach(userEvents, function (eventId, eventType) {
-                                var pointsForEventType = userEvents[eventType].length * eventTypes[eventType].points,
-                                    maxPointsForEventType = eventTypes[eventType].maxPoints;
-                                citizenshipPoints += (maxPointsForEventType !== 0 && pointsForEventType > maxPointsForEventType) ? maxPointsForEventType : pointsForEventType;
-                            });
-                            for (var roomswipeType in roomswipeEvents) {
-                                if (roomswipeEvents.hasOwnProperty(roomswipeType)){
-                                    roomswipePoints += Object.keys(roomswipeEvents[roomswipeType]).length;
-                                    if (roomswipePoints >= 10) {
-                                        citizenshipPoints += 1;
-                                        break;
-                                    }
-                                }
-                            }
-                            citizenship = { points: citizenshipPoints, events: userEvents, roomswipe: roomswipeEvents };
-                            defer.resolve(citizenship);
-                        });
-                        
-                    });
-                    
-                });
-                
-                return defer.promise;
-                
-            }
-            
-        };
+						citizenshipTypes[key] = {
+							enabled: typeIsEnabled,
+							points: typePoints,
+							maxPoints: typeMaxPoints
+						};
+						if (typeIsEnabled) {
+							citizenshipTypesEnabled[key] = {
+								points: typePoints,
+								maxPoints: typeMaxPoints
+							};
+						} else {
+							citizenshipTypesDisabled[key] = {
+								points: typePoints,
+								maxPoints: typeMaxPoints
+							};
+						}
+					});
+					types = {
+						types: citizenshipTypes,
+						enabledTypes: citizenshipTypesEnabled,
+						disabledTypes: citizenshipTypesDisabled
+					};
+					defer.resolve(types);
+				});
+				return defer.promise;
+			},
+			
+			getEvents: function (studentId) {
+				if (studentId == undefined) {
+					studentId = UserService.profile.id;
+				}
+				return SwipeService.listByUser(studentId);
+			},
+			
+			getPoints: function(studentId) {
+				if (studentId == undefined) {
+					studentId = UserService.profile.id;
+				}
+				var userEvents = citizenshipFactory.getEvents();
+				console.log(userEvents);
+				angular.forEach(userEvents, function(eventInfo, key) {
+					console.log('test');
+				});
+			}
+			
+		};
         
         
-        return citizenshipFactory;
+		return citizenshipFactory;
         
     }]);
 
+    
     angular.module("myhonorsCitizenship").filter('orderCitizenship', function() {
         return function(items, field, reverse) {
             var filtered = [];
